c1b2320e758f27c6a96cc513d5971496db575d21
YoungjaeRo <ryj0330@naver.com>
2024-12-26 15:55:40 +0900

    feat : application.yml 파일 Hibernate 배치 처리 설정 (size 100)

--- backend/src/main/java/cotato/backend/BackendApplication.java
+++ backend/src/main/java/cotato/backend/BackendApplication.java
@@ -2,8 +2,11 @@ package cotato.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EntityScan(basePackages = {"cotato.backend.domains.post.entity"})
 public class BackendApplication {
 
 	public static void main(String[] args) {
--- backend/src/main/java/cotato/backend/domains/post/entity/Post.java
+++ backend/src/main/java/cotato/backend/domains/post/entity/Post.java
@@ -1,5 +1,7 @@
 package cotato.backend.domains.post.entity;
 
+import org.springframework.boot.autoconfigure.web.WebProperties;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
